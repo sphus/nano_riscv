fb659fdb3cc71caeeddc1fc43415bb09092f4e0e
小段 <14004237+shu__ying@user.noreply.gitee.com>
2025-04-23 21:39:46 +0800

    优化control代码

--- rtl/core/control.v
+++ rtl/core/control.v
@@ -19,7 +19,7 @@ module control (
         output wire                 mem_sign    ,   // Load/Store Data Sign
         output wire                 sign        ,   // ALU SIGN
         output wire                 sub         ,   // ALU SUB
-        output reg  [`StateBus]     state
+        output wire [`StateBus]     state
         // output wire [`StateBus]     state
     );
 
@@ -38,30 +38,17 @@ module control (
     wire    inst_S  = (opcode == `INST_TYPE_S   );
     wire    jcc     = (opcode == `INST_TYPE_B   );
 
-    always @(posedge clk or negedge rstn)
-    begin
-        if (!rstn)
-            state <= `IF_STATE;
-        else if(hold)
-            state <= `IF_STATE;
-        else if (state[`EX] & ~inst_L)
-            state <= `IF_STATE;
-        else
-            state <= {state[`Statenum-2:0],state[`Statenum-1]};
-    end
 
-    // DFF #(`Statenum) STATE_DFF(
-    //     .clk      (clk          ),
-    //     .rstn     (rstn         ),
-    //     .CE       (~hold        ),
-    //     .set_data (`IF_STATE    ),
-    //     .d        ({state[`Statenum-2:0],state[`Statenum-1]}    ),
-    //     .q        (state        )
-    // );
+    wire [`StateBus] state_in = (state[`EX] & ~inst_L) ? `IF_STATE : {state[`Statenum-2:0],state[`Statenum-1]};
 
-
-
-
+    DFF #(`Statenum) STATE_DFF(
+            .clk      (clk      ),
+            .rstn     (rstn     ),
+            .CE       (~hold    ),
+            .set_data (`IF_STATE),
+            .d        (state_in ),
+            .q        (state    )
+        );
 
     // Jump signal
     assign imm_ctrl ={
@@ -70,8 +57,6 @@ module control (
                (lui    |auipc      ),   //  U
                (inst_S             ),   //  S
                (inst_I |inst_L|jalr)};  //  I
-
-
 
     wire    cjump_reg;
     wire    jal_reg  ;
@@ -83,8 +68,6 @@ module control (
     DFF #(1)CJUMP_DFF   (clk,rstn,`Enable,`Disable,cjump,cjump_reg  );
     DFF #(1)JAL_DFF     (clk,rstn,`Enable,`Disable,jal  ,jal_reg    );
     DFF #(1)JALR_DFF    (clk,rstn,`Enable,`Disable,jalr ,jalr_reg   );
-
-
 
     // ALU signal
     assign alu_ctrl = func3 & {`ALU_ctrl_num{(inst_I | inst_R | jcc) & (state[`EX]|state[`MEM])}};
@@ -99,14 +82,8 @@ module control (
            ({`LS_unsigned,`LS_W} & {3{state[`IF]}}) ;
 
     // register write enable
-    // assign wen  = (inst_I|
-    //                inst_R|
-    //                inst_L|
-    //                jal  |
-    //                jalr |
-    //                lui  |
-    //                auipc) & state[`MEM];
-    assign wen  = ((inst_I|inst_R|jal|jalr|lui|auipc) & state[`EX]) |
+    assign wen  =
+           ((inst_I|inst_R|jal|jalr|lui|auipc) & state[`EX]) |
            (inst_L & state[`MEM]);
 
     // assign wmem = inst_S & state[`MEM];
